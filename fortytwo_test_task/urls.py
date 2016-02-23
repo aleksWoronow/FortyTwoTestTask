cc30f5c4ea0fbdd7411ac3edc4bdebde6c86cfea
leksw <aleks.woronow@yandex.ru>
2016-02-23 18:44:07 +0000

    added bootstrap and mockup into django template

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('apps.hello.urls', namespace='hello')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
