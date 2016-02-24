13e7a80ae7280adebe909ab493a76958aacd1479
leksw <aleks.woronow@yandex.ru>
2016-02-24 21:06:43 +0000

    ticket:1 changed static_url and added view test

---
+++ apps/hello/tests/test_views.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.http import HttpRequest
+
+from ..views import home_page
+
+
+class HomePageViewTest(TestCase):
+
+    def test_home_page_view(self):
+        """Test view home_page"""
+        request = self.client.get(reverse('hello:home'))
+        response = home_page(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertIn('Aleks', response.content)
+        self.assertIn('Woronow', response.content)
+
+
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        """Test home page"""
+
+        response = self.client.get(reverse('hello:home'))
+
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response,
+                            '<h1>42 Coffee Cups Test Assignment</h1>',
+                            html=True)
+        self.assertContains(response, 'Aleks')
+        self.assertContains(response, 'Woronow')
+        self.assertContains(response, 'Feb. 25, 2016')
+        self.assertContains(response, 'aleks.woronow@yandex.ru')
+        self.assertContains(response, 'aleksw@42cc.co')
+        self.assertContains(response, 'I was born')
+
+    def test_home_page_returns_correct_html(self):
+        """Test home page returns correct html"""
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.strip().
+                        startswith(b'<!DOCTYPE html>'))
+        self.assertIn(b'<title>My card</title>', response.content)
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,5 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import date
+
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    context = {
+        'name': 'Aleks',
+        'surname': 'Woronow',
+        'date_of_birth': date(2016, 2, 25),
+        'bio': 'I was born ...',
+        'email': 'aleks.woronow@yandex.ru',
+        'jabber': 'aleksw@42cc.co',
+        'skype_id': 'aleks_woronow',
+        'other': '...'
+        }
+    return render(request, 'home.html', context)
