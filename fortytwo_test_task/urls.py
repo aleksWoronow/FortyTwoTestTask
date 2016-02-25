f025e5a9c5eae8b49508fba252edf03dfb03298e
leksw <aleks.woronow@yandex.ru>
2016-02-25 18:08:54 +0000

    ticket:1 added static to urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,3 +15,7 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
