85279a5d7b9657a23e99f8acb88daa25986af7b2
leksw <aleks.woronow@yandex.ru>
2016-03-01 20:49:34 +0000

    ticket:5 added widget to form

--- apps/hello/forms.py
+++ apps/hello/forms.py
@@ -2,23 +2,35 @@
 from __future__ import unicode_literals
 
 from django.forms import ModelForm
+from django import forms
 
 from .models import Person
+
+
+class CalendarWidget(forms.DateInput):
+    class Media:
+        js = ('http://code.jquery.com/ui/1.11.0/jquery-ui.js',)
+
+    def __init__(self, attrs={}):
+        super(CalendarWidget, self).__init__(
+            attrs={'class': 'form-control datepicker', 'size': '10'})
 
 
 class PersonForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(PersonForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            if field_name == 'date_of_birth':
-                field.widget.attrs['class'] = 'form-control datepicker'
-            else:
+            if field_name != 'date_of_birth':
                 field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Person
         fields = ['name', 'surname', 'date_of_birth', 'bio',
                   'email', 'jabber', 'skype_id', 'other']
+        widgets = {
+            'date_of_birth': CalendarWidget()
+          
+        }
 
     class Media:
-        js = ('js/person_form.js',)
+        js = ('js/change_person.js',)
