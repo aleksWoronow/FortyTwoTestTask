de1679106457a84ac08eb4057d7b8794b5aa449a
leksw <aleks.woronow@yandex.ru>
2016-03-02 19:44:45 +0000

    ticket:5 correct model str

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -2,11 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.conf import settings
 
 
-@python_2_unicode_compatible
 class Person(models.Model):
     name = models.CharField('name', max_length=250)
     surname = models.CharField('surname', max_length=250)
@@ -19,11 +17,10 @@ class Person(models.Model):
                                 max_length=250)
     other = models.TextField('other contact', blank=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return '%s %s' % (self.surname, self.name)
 
 
-@python_2_unicode_compatible
 class RequestStore(models.Model):
     path = models.CharField(max_length=250)
     method = models.CharField(max_length=10)
@@ -33,7 +30,7 @@ class RequestStore(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     new_request = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
+    def __unicode__(self):
         return "%s - %s" % (self.path, self.method)
 
     class Meta:
